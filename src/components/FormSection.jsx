756a22dbc4b55c3770e8ea38d1fed74c130edfc4
AC1D <moonwatcher445@gmail.com>
2026-04-07 11:25:00 -0400

    Update FormSection.jsx

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+export default function FormSection({ section, data, handleChange }) {
+  const inputs = Object.keys(data).map(key => (
+    <div key={key} style={{ marginBottom: 10 }}>
+      <label>{key}</label>
+      <input
+        type="text"
+        value={data[key]}
+        onChange={e => handleChange(section, key, e.target.value)}
+        style={{ width: "100%", padding: 5 }}
+      />
+    </div>
+  ));
+
+  return <div style={{ marginBottom: 20 }}>{inputs}</div>;
+}
